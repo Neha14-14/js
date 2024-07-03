4fec2a81d0d1e9069dfc589f4d536998a28cd383
Neha14-14 <98690015+Neha14-14@users.noreply.github.com>
2024-07-03 08:30:32 +0000

    variables

---
+++ .devcontainer/01_basics.js/variables.js
@@ -0,0 +1,9 @@
+const accountId = 12434
+let accountEmail = "neha@gmail.com"
+var accountPassword = "45666"
+accountCity = "Jalandhar"
+console.table([accountEmail,accountId,accountPassword,accountCity])
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
